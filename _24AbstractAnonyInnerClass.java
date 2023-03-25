85e8726b1c444a6c8f531ccb4ab7c3567bd76deb
pawan8979 <p.negi8979@gmail.com>
2023-03-25 23:16:40 +0530

    innerClass

---
+++ _24AbstractAnonyInnerClass.java
@@ -0,0 +1,39 @@
+abstract class A
+{
+	public abstract void show();
+	public abstract void config();
+
+}
+
+//class B extends A
+//{
+//	public void show()
+//	{
+//		System.out.println("in B show");
+//	}
+//}
+
+
+public class _24AbstractAnonyInnerClass{
+    public static void main(String[] args) {
+    	
+//    	A obj=new B();
+    	
+    	A obj=new A() 
+    	{
+    		public void show()
+    		{
+    			System.out.println("in new show");
+    		}
+
+            public void config() {
+                System.out.println("in new config");
+            }
+    	};
+    	obj.show();
+        obj.config();
+    	
+    }
+}
+
+
