f87eb26ff5a11b27b1eca7e0a55dce95fdcb425b
pawan8979 <p.negi8979@gmail.com>
2023-03-25 22:30:38 +0530

    new

---
+++ _19Final.java
@@ -0,0 +1,54 @@
+//final - variable(unchangeable), method(overriding blocker), class(inheritance blocker)
+
+//final class Calc
+//{
+//	public void show()
+//	{
+//		System.out.println("in Calc show");
+//	}
+//	public void add(int a, int b)
+//	{
+//		System.out.println(a+b);
+//	}
+//}
+
+
+class Calc
+{
+	public final void show()
+	{
+		System.out.println("By Navin");
+	}
+	public void add(int a, int b)
+	{
+		System.out.println(a+b);
+	}
+}
+
+class AdvCalc extends Calc
+{
+	public void show()
+	{
+		System.out.println("By John");
+	
+}
+
+	
+public class _19Final{
+    public static void main(String[] args) {
+        
+//    	final int num=8;
+//    	num=9;
+//    	System.out.println(num);
+    	
+//    	Calc obj= new Calc();
+//    	obj.show();
+//    	obj.add(4, 5);
+    	
+    	AdvCalc obj= new AdvCalc();
+    	obj.show();
+    	obj.add(4, 5);
+    }
+}
+
+ 
