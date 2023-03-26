e24064d844eb4ac9ef29da371c7337c272804bf6
pawan8979 <p.negi8979@gmail.com>
2023-03-26 12:54:31 +0530

    examtime

---
+++ _28Exception.java
@@ -0,0 +1,17 @@
+/* 
+Types of error:-
+1. Complile - time error
+2. Runtime error -> Exception handling
+3. Logical error
+*/
+
+public class _28Exception {
+    public static void main(String[] args) {
+    
+//  	System.out.Println();
+    	System.out.println(2+2);
+    	
+    }
+}
+
+
---
+++ _29TryCatch.java
@@ -0,0 +1,23 @@
+
+
+public class _29TryCatch {
+    public class Demo {
+        public static void main(String[] args) {
+        
+        int i=0;
+        int j=0;
+       
+        try
+        {
+            j=18/i;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong");
+        }
+        System.out.println(j);
+        
+        System.out.println("Bye");
+        }
+    }
+}
